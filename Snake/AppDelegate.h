b048cb79f33ae620d5d1d8b4ca73fc6fcd28cfa7
David <dryan31@gmail.com>
2011-11-13 16:00:01 -0600

    Initial Commit

---
+++ Snake/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Snake
+//
+//  Created by David Ryan on 11/13/11.
+//  Copyright (c) 2011 DePaul University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Snake/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Snake
+//
+//  Created by David Ryan on 11/13/11.
+//  Copyright (c) 2011 DePaul University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
